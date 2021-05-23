abbf2f6dc9578d199d254920e1c068bda9658360
sharlq <shehabakhader@gmail.com>
2021-05-23 22:39:48 +0300

    started working on sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/sidebar/sidebar'
+import Chat from './components/chat/Chat'
 
-function App() {
+const App =() => {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar/>
+      <Chat/>
     </div>
   );
-}
+} 
 
 export default App;
