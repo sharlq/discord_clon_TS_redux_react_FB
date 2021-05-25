7cfb115a1d3737285dd4aabe8d106d04b8030889
sharlq <shehabakhader@gmail.com>
2021-05-25 19:48:35 +0300

    add sign in and logout features

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import SidebarProfile from './SidebarProfile'
 import Server from './server/server'
 import Connection from './connection'
+import {useSelector} from 'react-redux'
+import {selectUser} from  '../../features/userSlice'
 
 
 const Sidebar = ({}) => {
+    const user = useSelector(selectUser);
     return (
         <div className="sidebar">
             <Server/>
             <Connection channel={"channel1"} server={"server name"} />
-            <SidebarProfile id="#1" name="shehab"/>
+            <SidebarProfile pic={user.photo} id={user.uid} name={user.displayName}/>
         </div>
     )
 }
